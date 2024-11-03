65dab08cca4b588efab794e6248868eebf12fb9c
peer3737 <petervanelsacker@gmail.com>
2024-11-03 20:40:37 +0100

    update with logging

--- src/main.py
+++ src/main.py
@@ -2,28 +2,38 @@ import requests
 import os
 import json
 import boto3
+import logging
 
 
-events = {
-    "CPC": {
-        "check": True,
-        "url": "https://nncpcloopdenhaag.nl/inschrijven/",
-        "open_text": "https://in.njuko.com"
-    },
-    "Utrecht marathon": {
-        "check": False,
-        "url": "https://utrechtmarathon.com/inschrijven/",
-        "open_text": "https://in.njuko.com"
-    }
-}
-
+formatter = logging.Formatter('[%(levelname)s] %(message)s')
+log = logging.getLogger()
+log.setLevel("INFO")
+logging.getLogger("boto3").setLevel(logging.WARNING)
+logging.getLogger("botocore").setLevel(logging.WARNING)
+for handler in log.handlers:
+    log.removeHandler(handler)
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+log.addHandler(handler)
 
 
 def lambda_handler(event, context):
     lambda_client = boto3.client('lambda')
-
+    events = {
+        "CPC": {
+            "check": True,
+            "url": "https://nncpcloopdenhaag.nl/inschrijven/",
+            "open_text": "https://in.njuko.com"
+        },
+        "Utrecht marathon": {
+            "check": True,
+            "url": "https://utrechtmarathon.com/inschrijven/",
+            "open_text": "https://in.njuko.com"
+        }
+    }
     for event in events:
         if events[event]['check']:
+            log.info
             response = requests.get(events[event]['url'])
             if events[event]['open_text'] in response.text:
                 payload = {

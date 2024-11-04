9a69fc10dfb722176a01e639155489e3e2441e07
peer3737 <petervanelsacker@gmail.com>
2024-11-04 08:55:35 +0100

    Add dynamodb dependency

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import os
 import json
 import boto3
 import logging
+from supporting import aws
 
 
 formatter = logging.Formatter('[%(levelname)s] %(message)s')
@@ -19,35 +20,26 @@ log.addHandler(handler)
 
 def lambda_handler(event, context):
     lambda_client = boto3.client('lambda')
-    events = {
-        "CPC": {
-            "check": True,
-            "url": "https://nncpcloopdenhaag.nl/inschrijven/",
-            "open_text": "https://in.njuko.com"
-        },
-        "Utrecht marathon": {
-            "check": True,
-            "url": "https://utrechtmarathon.com/inschrijven/",
-            "open_text": "https://in.njuko.com"
-        }
-    }
+    events = aws.dynamodb_query('events')
     for event in events:
-        if events[event]['check']:
-            log.info(event)
-            response = requests.get(events[event]['url'])
-            if events[event]['open_text'] in response.text:
-                payload = {
-                    "to": os.environ['MAIL_CONTACT'],
-                    "from": os.environ['MAIL_SENDER'],
-                    "subject": f"Inschrijving voor {event} is geopend",
-                    "content": f"Inschrijving voor {event} is geopend. Ga naar {events[event]['url']}"
-                }
+        event_id = event['id']
+        event_name = event['name']
+        log.info(event_name)
+        response = requests.get(event['url'])
+        if event['open_text'] in response.text:
+            payload = {
+                "to": os.environ['MAIL_CONTACT'],
+                "from": os.environ['MAIL_SENDER'],
+                "subject": f"Inschrijving voor {event_name} is geopend",
+                "content": f"Inschrijving voor {event_name} is geopend. Ga naar {event['url']}"
+            }
 
-                lambda_client.invoke(
-                    FunctionName='sendMail',  # Replace with the name of your sendMail function
-                    InvocationType='Event',  # Use 'RequestResponse' for synchronous invocation
-                    Payload=json.dumps(payload)
-                )
+            lambda_client.invoke(
+                FunctionName='sendMail',  # Replace with the name of your sendMail function
+                InvocationType='Event',  # Use 'RequestResponse' for synchronous invocation
+                Payload=json.dumps(payload)
+            )
+            aws.dynamo_db_update('events', item_id=event_id, attribute='check', value=False)
     return None
 
 
